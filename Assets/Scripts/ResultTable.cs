d3643ea6e04c1e2d48db88a2c723128f998d2f91
Андрей Митенин <andrejmitenin@MacBook-Air-Andrej.local>
2025-02-08 19:24:07 +0300

    доделал сортировку чисел в массиве

--- Assets/Scripts/ResultTable.cs
+++ Assets/Scripts/ResultTable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,13 +18,14 @@ public class ResultTable : MonoBehaviour
         }
         set
         {
-            if(value.Length == length)
+            if (value.Length == length)
             {
                 _names = value;
             }
             UpdateText();
         }
     }
+
     public int[] points
     {
         get
@@ -32,7 +34,7 @@ public class ResultTable : MonoBehaviour
         }
         set
         {
-            if(value.Length == length)
+            if (value.Length == length)
             {
                 _points = value;
             }
@@ -47,15 +49,39 @@ public class ResultTable : MonoBehaviour
 
     void UpdateText()
     {
-        int[] positions = new int[10]{9,8,7,6,5,4,3,2,1,0};
+
         string[] sorted_entries = new string[10];
+        //перебор каждого элемента массива не начиная с нуля, а с int a
+        int kl = 0;
+        int a = 0;
 
+        //предназначены чтлбы поменять элементы местами
+        int copyElement;
+        string copyName;
+        for (int i = 0; i < 10; i++)
+        {
+            for (kl = a; kl < 10; kl++)
+            {
+                if (_points[i] < _points[kl])
+                {
+                    //меняем элементы массива(очки и имя)
+                    copyElement = _points[i];
+                    copyName = _names[i];
 
+                    _points[i] = _points[kl];
+                    _names[i] = _names[kl];
+
+                    _names[kl] = copyName;
+                    _points[kl] = copyElement;
+                }
+            }
+            a++;
+        }
 
         int counter = 0;
         foreach (string entry in sorted_entries)
         {
-            sorted_entries[counter] = _names[positions[counter]] + ": " + _points[positions[counter]];
+            sorted_entries[counter] = _names[counter] + ": " + _points[counter];
             counter++;
         }
 
